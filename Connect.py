ec8de3e943e7453408175a65425c1466ba1ccff6
vhfmaia <vhfmaia>
2022-04-11 15:50:22 +0100

    Rework the code, not it works, hopefully :)

---
+++ Connect.py
@@ -0,0 +1,25 @@
+import glob
+import os
+import sys
+import time
+
+try:
+ sys.path.append(glob.glob('./PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
+ sys.version_info.major,
+ sys.version_info.minor,
+ 'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+import carla
+
+client = carla.Client('localhost', 2000)
+client.set_timeout(2.0)
+world = client.get_world()
+blueprint_library = world.get_blueprint_library()
+bp = blueprint_library.filter('model3')[0]
+world = client.get_world()
+spawnPoint=carla.Transform(carla.Location(x=38.6,y=5.8, z=0.598),carla.Rotation(pitch=0.0, yaw=0.0, roll=0.000000))
+vehicle = world.spawn_actor(bp, spawnPoint)
+time.sleep(10)
+vehicle.destroy()
