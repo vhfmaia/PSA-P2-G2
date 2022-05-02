49d4faa046a2de8c612fd372f39acb62365983a0
joao <jpsalmeida@ua.pt>
2022-05-02 15:19:46 +0100

    Code camera

--- Camera carro.py
+++ Camera carro.py
@@ -1,5 +1,6 @@
 camera = carla.sensor.Camera('MyCamera', PostProcessing='SceneFinal')
-#camera configuratuion
+
+#camera configuration
 camera.set(bloom_intensity=0.675)
 camera.set(FOV=90.0)
 camera.set_image_size(800, 600)
@@ -9,6 +10,13 @@ camera.set(iso=100)
 camera.set(gamma=2.2)
 camera.set(sensor_tick=0.5)
 camera.set(shutter_speed=60)
+
 #Camera lens distortion attributes
+camera.set(lens_circle_falloff=5.0)
+camera.set(lens_circle_multiplier=0.0)
+camera.set(lens_k=-1.0)
+camera.set(lens_kcube=0.0)
+camera.set_lens_size(x=0.08, y=0.08)
+
 
 carla_settings.add_sensor(camera)
