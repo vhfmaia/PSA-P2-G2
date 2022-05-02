76e28ee538958f6c8b654baef6109c609c54171c
joao <jpsalmeida@ua.pt>
2022-05-02 14:52:15 +0100

    Code camera

--- Camera carro.py
+++ Camera carro.py
@@ -5,5 +5,8 @@ camera.set_image_size(800, 600)
 camera.set_position(x=0.30, y=0, z=1.30)
 camera.set_rotation(pitch=0, yaw=0, roll=0)
 camera.set(iso=100)
+camera.set(gamma=2.2)
+camera.set(sensor_tick=0.5)
+camera.set(shutter_speed=60)
 
 carla_settings.add_sensor(camera)
