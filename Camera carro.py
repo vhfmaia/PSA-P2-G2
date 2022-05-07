e6e7ddd8e1e35c812040119141c95fe7117aee6f
joao <jpsalmeida@ua.pt>
2022-05-07 13:52:41 +0100

    Code wheel

--- Camera carro.py
+++ Camera carro.py
@@ -4,14 +4,14 @@ import numpy as np
 import pygame
 import random
 
-client = carla.client('localhost',2000)
-client.set_timeout(2.0)
-world=client.load_wold("Town02")
+client = carla.Client('localhost',2000)
+client.set_timeout(60.0)
+world = client.load_world("Town02")
 
 world.set_weather(carla.WeatherParameters.ClearNoon)
 
-bp=world.get_blueprint_library()
-vehicle_bp=bp.filter('vehicle.tesla.model3') [0]
+bp = world.get_blueprint_library()
+vehicle_bp = bp.filter('vehicle.tesla.model3') [0]
 print(vehicle_bp)
 
 spawn_point = random.choice(world.get_map().get_spawn_points())
@@ -30,29 +30,29 @@ transform.location.yaw=180
 transform.location.pitch=0
 transform.location.roll=0
 
-vehicle=world.spaw_actor(vehicle_bp, transform)
+vehicle = world.spawn_actor(vehicle_bp, transform)
 
-spectator=world.get_spectator()
-sp_transform=carla.Transform(transform.location + carla.Location(z=30, x=-25))
+spectator = world.get_spectator()
+sp_transform = carla.Transform(transform.location + carla.Location(z=30, x=-25))
 carla.Rotation(yaw=90, pitch=-90)
-spectator.set_tranform(sp_transform)
+spectator.set_transform(sp_transform)
 
 control=carla.VehicleControl()
 control.throttle=0.3
 vehicle.apply_control(control)
 
-rgb_camera_bp=world.get_blueprint_library().find('sensor.camera.rgb)')
+rgb_camera_bp=world.get_blueprint_library().find('sensor.camera.rgb')
 # rgb_camera_bp.set_attribute("image_size_X", f"{IM_WIDTH}")
 # rgb_camera_bp.set_attribute("image_size_Y", f"{IM_HEIGHT}")
 
 cam_transform=carla.Transform(carla.Location(x=0.8, z=1.7))
-Camera=world.spaw_actor(rgb_camera_bp,
+Camera = world.spawn_actor(rgb_camera_bp,
                  cam_transform,
                  attach_to=vehicle,
                  attachment_type=carla.AttachmentType.Rigid)
 
 def handle_image(image):
-    #image.save_to_disk('output/%05d.png' % image.frame, carla.ColorConvert.Raw)
+    image.save_to_disk('output/%05d.png' % image.frame, carla.ColorConverter.Raw)
     org_array=np.frombuffer(image.raw_data, dtype=np.disp('uint8'))
     array=np.reshape(org_array, (image.height, image.width, 4))
     array=array[:, :, :3]
@@ -66,11 +66,11 @@ def handle_image(image):
 display= pygame.display.set_mode(
         (1200, 600), pygame.HWSURFACE | pygame.DOUBLEBUF)
 
-rgb_camera_bp.listen(lambda image: handle_image(display, image))
+Camera.listen(lambda image: handle_image(display, image))
 
 time.sleep(15)
 
-rgb_camera_bp.destroy()
+Camera.destroy()
 vehicle.destroy()
 
 # camera = carla.sensor.Camera('MyCamera', PostProcessing='SceneFinal')
