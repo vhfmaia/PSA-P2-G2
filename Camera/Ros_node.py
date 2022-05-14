271c76ff84ad4cfd06743f0698e8fa15614b94e5
joao <jpsalmeida@ua.pt>
2022-05-14 16:18:42 +0100

    Code ROS node

--- Camera/Ros_node.py
+++ Camera/Ros_node.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python3
 
 import rospy
-from camera_carro import handle_image
-from camera_carro import display
-from camera_carro import image
+import cv2
+import os
+import numpy as np
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+bridge = CvBridge
+def image_callback(msg):
+    print("Received an image!")
+    try:
+        # Convert your ROS Image message to OpenCV2
+        cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+    except CvBridgeError, e:
+        print(e)
+    else:
+        # Save your OpenCV2 image as a jpeg
+        cv2.imwrite('camera_image.jpeg', cv2_img)
 
 def camera():
     rospy.init_node("camera_images")
-
-    loop_rate = rospy.Rate(10)
-
-    images = rospy.Publisher(handle_image(display, image))
+    rate = rospy.Rate(10)
+    images = rospy.Publisher("image_Carla", Image, queue_size=10)
+    rospy.Subscriber(images, Image, image_callback)
 
     while not rospy.is_shutdown():
-        loop_rate.sleep()
+        rate.sleep()
+
 
 
 if __name__ == '__main__':
