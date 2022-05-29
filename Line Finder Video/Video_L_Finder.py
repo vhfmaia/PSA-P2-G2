37f31427c3a008c006c373ccaddb1b8b82165fb8
victor <vhfm@ua.pt>
2022-05-29 18:54:11 +0100

    ...

--- Line Finder Video/Video_L_Finder.py
+++ Line Finder Video/Video_L_Finder.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 
 video = cv2.VideoCapture("Input.mp4")
+W = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+H = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print(W, H)
 
 def ROI(img, vertices):
     mask = np.zeros_like(img)
@@ -10,6 +13,17 @@ def ROI(img, vertices):
     cv2.fillPoly(mask, vertices, match_mask_color)
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
+
+
+def transform(input_t):
+    _, input_t, = video.read()
+
+    pts1 = np.float32([[0.57*W, 0.51*H], [0.95*W, 0.92*H],[0.44*W, 0.51*H], [0.05*W, 0.92*H]])
+    pts2 = np.float32([[600, 0], [600, 400], [0, 0], [0, 400]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+
+    result = cv2.warpPerspective(frame, matrix, (600, 400))
+    return result
 
 
 while True:
@@ -21,19 +35,20 @@ while True:
     W = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
     H = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    Limits = [((5/100) * W, (92/100) * H), ((44/100) * W, (519/1000) * H), ((56/100) * W, (519/1000) * H), ((95/100) * W, (92/100) * H)]
-    blur = cv2.GaussianBlur(frame, (7,7), 0)
+    Limits = [((5 / 100) * W, (92 / 100) * H), ((44 / 100) * W, (519 / 1000) * H), ((56 / 100) * W, (519 / 1000) * H),
+              ((95 / 100) * W, (92 / 100) * H)]
+    blur = cv2.GaussianBlur(frame, (7, 7), 0)
     gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-    roi1 = ROI(gray, np.array([Limits], np.int32))
+    roi1 = transform(ROI(gray, np.array([Limits], np.int32)))
     kernel = np.array([[0, -1, 0],
                        [-1, 5, -1],
                        [0, -1, 0]])
-    roi3 = cv2.filter2D(src=roi1, ddepth=-5, kernel=kernel)
+    roi3 = transform((cv2.filter2D(src=roi1, ddepth=-5, kernel=kernel)))
     roi2 = cv2.Canny(image=roi1,
-                      threshold1=300,
-                      threshold2=300,
-                      apertureSize=3,
-                      L2gradient=False)
+                     threshold1=300,
+                     threshold2=300,
+                     apertureSize=3,
+                     L2gradient=False)
 
     canny = cv2.Canny(image=gray,
                       threshold1=300,
@@ -44,7 +59,7 @@ while True:
     roi = ROI(blur, np.array([Limits], np.int32))
     lines = cv2.HoughLinesP(roi,
                             rho=1,
-                            theta=np.pi/180,
+                            theta=np.pi / 180,
                             threshold=100,
                             lines=np.array([]),
                             minLineLength=1,
@@ -52,26 +67,27 @@ while True:
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line[0]
-            cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), thickness=1)
+            cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), thickness=1
+                     )
 
     lines1 = cv2.HoughLinesP(roi,
-                            rho=1,
-                            theta=np.pi / 180,
-                            threshold=100,
-                            lines=np.array([]),
-                            minLineLength=1,
-                            maxLineGap=1)
+                             rho=1,
+                             theta=np.pi / 180,
+                             threshold=100,
+                             lines=np.array([]),
+                             minLineLength=1,
+                             maxLineGap=1)
     if lines1 is not None:
         for line in lines1:
             x1, y1, x2, y2 = line[0]
             cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), thickness=1)
 
-    final = cv2.GaussianBlur(frame, (3,3), 0)
-    cv2.imshow("output1", roi3)
+    final = cv2.GaussianBlur(frame, (3, 3), 0)
+    cv2.imshow("output1", roi1)
     cv2.imshow("output0", final)
     cv2.imshow("output2", canny)
     key = cv2.waitKey(27)
-    if key==27:
+    if key == 27:
         break
 
 video.release()
