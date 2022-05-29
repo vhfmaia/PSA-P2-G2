2843cc1523253cf39b59be04afc4c76e43c2f97c
victor <vhfm@ua.pt>
2022-05-29 23:50:57 +0100

    ...

---
+++ Driver_Example/driver_example.py
@@ -0,0 +1,90 @@
+#! /usr/bin/python3
+
+# rospy for the subscriber
+import rospy
+import numpy as np
+# OpenCV2 for saving an image
+import cv2
+from geometry_msgs.msg import Twist
+# ROS Image message
+from sensor_msgs.msg import Image
+
+# ROS Image message -> OpenCV2 image converter
+from cv_bridge import CvBridge, CvBridgeError
+
+# Global variables
+# Instantiate CvBridge
+bridge = CvBridge()
+publisher = None
+
+
+def imageCallback(msg):
+    print("Received an image!")
+
+    try:
+        # Convert your ROS Image message to OpenCV2
+        img = bridge.imgmsg_to_cv2(msg, "bgr8")
+    except:
+        print('Could not convert image')
+        return
+
+    h = img.shape[0]
+    w = img.shape[1]
+    print('height:', h, '\nwidth:', w, '\nchannel count:', img.shape[2], '\n')
+    vertices = [
+        (0, h),
+        (w * 0.35, h * 0.3),
+        (w * 0.65, h * 0.3),
+        (w, h)
+    ]
+    mask = np.zeros_like(img)
+    match = (255,) * img.shape[2]
+    cv2.fillPoly(mask, np.array([vertices], np.int32), match)
+    masked = cv2.bitwise_and(img, mask)
+    gray = cv2.cvtColor(masked, cv2.COLOR_RGB2GRAY)
+    canny = cv2.Canny(gray, threshold1=300, threshold2=300, apertureSize=3, L2gradient=False)
+
+    lines = cv2.HoughLinesP(canny,
+                            rho=1,
+                            theta=np.pi / 90,
+                            threshold=5,
+                            lines=np.array([]),
+                            minLineLength=1,
+                            maxLineGap=1)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), thickness=3)
+    # Save your OpenCV2 image as a jpeg
+    cv2.imshow('ROI', canny)
+    cv2.imshow('front camera', img)
+    cv2.waitKey(20)
+    # How to process the image to define the best angle and speed?
+
+    # make a driving decision
+    angle = 0
+    speed = 0
+
+    # build a twist msg to publish
+    twist = Twist()
+    twist.linear.x = speed
+    twist.angular.z = angle
+    global publisher
+    # publisher.publish(twist)
+
+
+def main():
+    rospy.init_node('driver')
+    # Define your image topic
+    image_topic = "/front_camera/rgb/image_raw"
+    # Set up your subscriber and define its callback
+    rospy.Subscriber(image_topic, Image, imageCallback)
+    global publisher
+    publisher = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+
+    # Spin until ctrl + c
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
--- Line Finder Video/Video_L_Finder.py
+++ Line Finder Video/Video_L_Finder.py
@@ -1,10 +1,22 @@
 import cv2
 import numpy as np
 
-video = cv2.VideoCapture("Input.mp4")
+video = cv2.VideoCapture("lines.mp4")
 W = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
 H = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 print(W, H)
+
+
+# roi1 = transform(ROI(gray, np.array([Limits], np.int32)))
+# kernel = np.array([[0, -1, 0],
+#                    [-1, 5, -1],
+#                    [0, -1, 0]])
+# roi3 = transform((cv2.filter2D(src=roi1, depth=-5, kernel=kernel)))
+# roi2 = cv2.Canny(image=roi1,
+#                  threshold1=300,
+#                  threshold2=300,
+#                  apertureSize=3,
+#                  L2gradient=False)
 
 def ROI(img, vertices):
     mask = np.zeros_like(img)
@@ -18,7 +30,7 @@ def ROI(img, vertices):
 def transform(input_t):
     _, input_t, = video.read()
 
-    pts1 = np.float32([[0.57*W, 0.51*H], [0.95*W, 0.92*H],[0.44*W, 0.51*H], [0.05*W, 0.92*H]])
+    pts1 = np.float32([[0.57 * W, 0.51 * H], [0.95 * W, 0.92 * H], [0.44 * W, 0.51 * H], [0.05 * W, 0.92 * H]])
     pts2 = np.float32([[600, 0], [600, 400], [0, 0], [0, 400]])
     matrix = cv2.getPerspectiveTransform(pts1, pts2)
 
@@ -28,64 +40,43 @@ def transform(input_t):
 
 while True:
     ret, frame = video.read()
-    if not ret:
-        video = cv2.VideoCapture("road_car_view.mp4")
-        continue
-
     W = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
     H = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    Limits = [((5 / 100) * W, (92 / 100) * H), ((44 / 100) * W, (519 / 1000) * H), ((56 / 100) * W, (519 / 1000) * H),
-              ((95 / 100) * W, (92 / 100) * H)]
+    Limits = [((0 / 100) * W, (100 / 100) * H), ((35 / 100) * W, (25 / 100) * H), ((65 / 100) * W, (25 / 100) * H),
+              ((100 / 100) * W, (100 / 100) * H)]
     blur = cv2.GaussianBlur(frame, (7, 7), 0)
     gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-    roi1 = transform(ROI(gray, np.array([Limits], np.int32)))
-    kernel = np.array([[0, -1, 0],
-                       [-1, 5, -1],
-                       [0, -1, 0]])
-    roi3 = transform((cv2.filter2D(src=roi1, ddepth=-5, kernel=kernel)))
-    roi2 = cv2.Canny(image=roi1,
-                     threshold1=300,
-                     threshold2=300,
-                     apertureSize=3,
-                     L2gradient=False)
-
     canny = cv2.Canny(image=gray,
                       threshold1=300,
                       threshold2=300,
                       apertureSize=5,
                       L2gradient=False)
-    blur = cv2.GaussianBlur(canny, (7, 7), 0)
-    roi = ROI(blur, np.array([Limits], np.int32))
-    lines = cv2.HoughLinesP(roi,
-                            rho=1,
-                            theta=np.pi / 180,
-                            threshold=100,
-                            lines=np.array([]),
-                            minLineLength=1,
-                            maxLineGap=1)
+    roi = ROI(canny, np.array([Limits], np.int32))
+    lines = cv2.HoughLines(roi, rho=2, theta=np.pi / 30, threshold=100, lines=np.array([]),
+                           srn=None, stn=None, min_theta=0 * np.pi / 180, max_theta=10 * np.pi / 180)
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line[0]
-            cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), thickness=1
+            cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), thickness=3
                      )
 
-    lines1 = cv2.HoughLinesP(roi,
-                             rho=1,
-                             theta=np.pi / 180,
-                             threshold=100,
-                             lines=np.array([]),
-                             minLineLength=1,
-                             maxLineGap=1)
-    if lines1 is not None:
-        for line in lines1:
-            x1, y1, x2, y2 = line[0]
-            cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), thickness=1)
+    # lines1 = cv2.HoughLinesP(roi,
+    #                          rho=10,
+    #                          theta=np.pi / 360,
+    #                          threshold=100,
+    #                          lines=np.array([]),
+    #                          minLineLength=100,
+    #                          maxLineGap=100)
+    # if lines1 is not None:
+    #     for line in lines1:
+    #         x1, y1, x2, y2 = line[0]
+    #         cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), thickness=2)
 
     final = cv2.GaussianBlur(frame, (3, 3), 0)
-    cv2.imshow("output1", roi1)
+    cv2.imshow("output1", roi)
     cv2.imshow("output0", final)
-    cv2.imshow("output2", canny)
+    # cv2.imshow("output2", canny)
     key = cv2.waitKey(27)
     if key == 27:
         break
