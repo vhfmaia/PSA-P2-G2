258a37aca19f5ab9fca8fd1ef6fe6b534de7c5da
joao <jpsalmeida@ua.pt>
2022-05-29 19:11:45 +0100

    Code ROS node to image

--- Camera/Ros_node.py
+++ Camera/Ros_node.py
@@ -1,27 +1,64 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 
+# rospy for the subscriber
 import rospy
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
+
+# OpenCV2 for saving an image
 import cv2
+from geometry_msgs.msg import Twist
 
+# ROS Image message
+from sensor_msgs.msg import Image
+
+# ROS Image message -> OpenCV2 image converter
+from cv_bridge import CvBridge, CvBridgeError
+
+# Global variables
+# Instantiate CvBridge
 bridge = CvBridge()
+publisher = None
 
-def image_callback(msg):
+
+def imageCallback(msg):
     print("Received an image!")
+
     try:
         # Convert your ROS Image message to OpenCV2
         cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
-    except CvBridgeError, e:
-        print(e)
-    else:
-        # Save your OpenCV2 image as a jpeg
-        cv2.imwrite('camera_image.jpeg', cv2_img)
+    except:
+        print('Could not convert image')
+        return
 
+    # Save your OpenCV2 image as a jpeg
+    cv2.imshow('front_camera', cv2_img)
+    cv2.waitKey(20)
+
+    # How to process the image to define the best angle and speed?
+
+    # make a driving decision
+    angle = 0
+    speed = 0.1
+
+    # build a twist msg to publish
+    twist = Twist()
+    twist.linear.x = speed
+    twist.angular.z = angle
+    global publisher
+    publisher.publish(twist)
+
+
+def main():
+    rospy.init_node('driver')
+    # Define your image topic
+    image_topic = "/front_camera/rgb/image_raw"
+    # Set up your subscriber and define its callback
+    rospy.Subscriber(image_topic, Image, imageCallback)
+    global publisher
+    publisher = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+
+    # Spin until ctrl + c
+    rospy.spin()
 
 
 if __name__ == '__main__':
-        rospy.init_node('image_listener')
-        image_topic = "/carla/ego vehicle/rgb front/image"
-        rospy.Subscriber(image_topic, Image, image_callback)
-        rospy.spin()
+    main()
